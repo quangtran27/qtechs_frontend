73544489daa74a74a4da8b63509893323df51298
Quang Tran <quangtv2702@gmail.com>
2023-05-27 19:08:30 +0700

    update import App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import GlobalStyles from '~/components/GlobalStyles'
-import App from './App'
+import App from '~/App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
